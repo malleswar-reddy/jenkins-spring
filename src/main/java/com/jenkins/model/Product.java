043e398b3962cda99646106b42a3ca426d9cfa05
malleswar.mca <malleswar.mca@gmail.com>
2025-04-26 14:50:20 +0530

    sample spring boot jenkins test case

--- src/main/java/com/jenkins/model/Product.java
+++ src/main/java/com/jenkins/model/Product.java
@@ -1,11 +1,12 @@
 package com.jenkins.model;
 
+import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.Data;
 
 @Data
-@Table
+@Entity
 public class Product {
     @Id
     private Long id;
--- src/test/java/com/jenkins/SpringTodoApplicationTests.java
+++ src/test/java/com/jenkins/SpringTodoApplicationTests.java
@@ -2,12 +2,14 @@ package com.jenkins;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test") // 👈 Add this annotation
 class SpringTodoApplicationTests {
 
 	@Test
 	void contextLoads() {
 	}
-
 }
+
